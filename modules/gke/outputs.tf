efbadd7ffcfa7b9285e1457c2a36c3bb148a1837
elitamsut <e.tamsut@gmail.com>
2024-11-11 09:27:23 +0200

    Update outputs.tf

--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -1,26 +1,31 @@
-output "client_key" {
-  value = google_container_cluster.primary.master_auth.0.client_key
-  description = "Client key for GKE cluster"
-}
-
-output "cluster_ca_certificate" {
-  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-  description = "CA certificate for GKE cluster"
-}
-
-output "endpoint" {
-  value = google_container_cluster.primary.endpoint
-  description = "The endpoint for the GKE cluster"
-}
-# In modules/gke/main.tf (if not already there)
 output "cluster_id" {
-  value = google_container_cluster.primary.id
+  description = "an identifier for the resource with format projects/{{project}}/locations/{{zone}}/clusters/{{name}}"
+  value       = google_container_cluster.gke.id
 }
 
 output "cluster_master_version" {
-  value = google_container_cluster.primary.master_version
+  description = "The current version of the master in the cluster. This may be different than the min_master_version set in the config if the master has been updated by GKE."
+  value       = google_container_cluster.gke.master_version
 }
 
 output "client_certificate" {
-  value = google_container_cluster.primary.master_auth.0.client_certificate
+  description = "Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster."
+  value       = google_container_cluster.gke.master_auth.0.client_certificate
+  sensitive   = true
 }
+
+output "client_key" {
+  description = "Base64 encoded private key used by clients to authenticate to the cluster endpoint."
+  value       = google_container_cluster.gke.master_auth.0.client_key
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64 encoded public certificate that is the root certificate of the cluster."
+  value       = google_container_cluster.gke.master_auth.0.cluster_ca_certificate
+  sensitive   = true
+}
+
+output "endpoint" {
+  description = "The IP address of this cluster's Kubernetes master."
+  value       = google_container_cluster.gke.endpoint
